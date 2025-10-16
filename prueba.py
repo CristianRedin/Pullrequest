6055c5f2a04eebbf6a101e34a74d463c81cf78ec
CristianRedin <kristianredin@gmail.com>
2025-10-16 01:48:41 -0500

    avances

---
+++ prueba.py
@@ -0,0 +1,16 @@
+# Mostrar texto
+print("Hola, este es mi primer print en Python")
+
+# Mostrar números
+print(25)
+
+# Mostrar varias cosas
+nombre = "Kristian"
+edad = 22
+print("Mi nombre es", nombre, "y tengo", edad, "años")
+
+# Usando formato
+print(f"Mi nombre es {nombre} y tengo {edad} años")
+
+# Mostrar el resultado de una operación
+print("La suma de 5 + 3 es:", 5 + 3)
